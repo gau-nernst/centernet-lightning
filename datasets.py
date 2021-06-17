a3253770ce8e035b018b9fd5e2e130202617854a
Thien Tran <gau.nernst@yahoo.com.sg>
2021-06-17 15:08:45 +0800

    refactor datasets
    - use lightning Data Module for training
    - add imagenet normalization
    - add InferenceDataset for inference

--- datasets.py
+++ datasets.py
@@ -1,179 +1,115 @@
-from albumentations.core.serialization import save
-import cv2
+import warnings
+from typing import Dict, Iterable
 import os
 import pickle
-import copy
-from pycocotools.coco import COCO
+import json
 from collections import OrderedDict
 
-from typing import List
-
+import cv2
+import numpy as np
 import torch
-from torch import nn
-from torch.utils.data import Dataset
-import torchvision
+from torch.utils.data import Dataset, DataLoader
+import pytorch_lightning as pl
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+from pycocotools.coco import COCO
 
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
 
-class CenterNetDataset(Dataset):
-    def __init__(self, img_dir: str) -> None:
-        super(CenterNetDataset, self).__init__()
-        self.img_dir = img_dir
-        self.imgs = os.listdir(img_dir)
-        self.imgs.sort()
-
-    def __getitem__(self, idx: int):
-        img = os.path.join(self.img_dir, self.imgs[idx])
-        img = torchvision.io.read_image(img).float() / 255.
-
-        item = {
-            "image": img,
-            "size": None,
-            "offset": None,
-            "displacement": None
-        }
-        return item
-
-    def __len__(self):
-        return len(self.img_dir)
-
-def prepare_coco_detection(data_dir, coco_name):
+def prepare_coco_detection(data_dir: str, coco_version: str):
     ann_dir = os.path.join(data_dir, "annotations")
-    ann_file = os.path.join(ann_dir, f"instances_{coco_name}.json")
+    ann_file = os.path.join(ann_dir, f"instances_{coco_version}.json")
 
-    save_dir = os.path.join(ann_dir, coco_name)
+    save_dir = os.path.join(ann_dir, coco_version)
     os.makedirs(save_dir, exist_ok=True)
     detection_file = os.path.join(save_dir, "detections.pkl")
-    label_map_file = os.path.join(save_dir, "label_map.pkl")
-    id_map_file = os.path.join(save_dir, "id_map.pkl")
+    label_to_name_file = os.path.join(save_dir, "label_to_name.json")
+    label_to_id_file = os.path.join(save_dir, "label_to_id.json")
 
     # if already exist on disk, don't do anything
-    if os.path.exists(detection_file) and os.path.exists(label_map_file):
+    if os.path.exists(detection_file) and os.path.exists(label_to_name_file) and os.path.exists(label_to_id_file):
         return
 
     # extract only bboxes and ids data, otherwise train set annotations is too large
-    else:
-        coco = COCO(ann_file)
-
-        # since COCO 2017 have missing ids, re-map the ids to class labels
-        categories = OrderedDict(coco.cats)
-        id_to_label = {}
-        label_to_name = {}
-        for i,(k,v) in enumerate(categories.items()):
-            id_to_label[v["id"]] = i
-            label_to_name[i] = v["name"]
-
-        with open(label_map_file, "wb") as f:
-            pickle.dump(label_to_name, f)     # save to disk
-
-        with open(id_map_file, "wb") as f:
-            pickle.dump(id_to_label, f)
-
-        img_ids = coco.getImgIds()                          # list of all image ids
-        img_info = coco.loadImgs(img_ids)                   # list of dictionary
-        img_names = [x["file_name"] for x in img_info]      # we only need file_name
-        img_widths = [x["width"] for x in img_info]
-        img_heights = [x["height"] for x in img_info]
-
-        annotate_ids = [coco.getAnnIds(imgIds=x) for x in img_ids]      # get annotations for each image
-        annotates = [coco.loadAnns(ids=x) for x in annotate_ids]        
-
-        bboxes = []
-        labels = []
-        for ann in annotates:       # outer loop is loop over images
-            img_bboxes = []
-            img_labels = []
-            for detection in ann:   # inner loop is loop over detections in an image
-                bbox = detection["bbox"]
-                cat_id = detection["category_id"]
-
-                bbox[2] = max(bbox[2], 1)   # clip width and height to 1
-                bbox[3] = max(bbox[3], 1)
-            
-                img_bboxes.append(bbox)
-                img_labels.append(id_to_label[cat_id])
-
-            bboxes.append(img_bboxes)
-            labels.append(img_labels)
-
-        # bboxes = [[x["bbox"] for x in ann] for ann in annotates]        # we only need bboxes and category_id
-        # labels = [[id_to_label[x["category_id"]] for x in ann] for ann in annotates]
-        
-        detection = {
-            "img_ids": img_ids,
-            "img_names": img_names,
-            "img_widths": img_widths,
-            "img_heights": img_heights,
-            "bboxes": bboxes,
-            "labels": labels
-        }
-        with open(detection_file, "wb") as f:
-            pickle.dump(detection, f)          # save to disk
-
-        detection["label_to_name"] = label_to_name
-
-        del coco
+    coco = COCO(ann_file)
+    categories = OrderedDict(coco.cats)
     
+    label_to_id = {0: 0}    # 0 is background class
+    label_to_name = {0: "background"}
+    for i, v in enumerate(categories.values()):
+        label_to_id[i+1] = v["id"]
+        label_to_name[i+1] = v["name"]
+    id_to_label = {v: k for k, v in label_to_id.items()}
+
+    # save to disk
+    with open(label_to_name_file, "w") as f:
+        json.dump(label_to_name, f)
+    with open(label_to_id_file, "w") as f:
+        json.dump(label_to_id, f)
+
+    img_ids = coco.getImgIds()                          # list of all image ids
+    img_info = coco.loadImgs(img_ids)                   # list of img info, each is a dict
+    img_names = [x["file_name"] for x in img_info]      # we only need file_name to open the images
+
+    annotate_ids = [coco.getAnnIds(imgIds=x) for x in img_ids]      # get annotations for each image
+    annotates = [coco.loadAnns(ids=x) for x in annotate_ids]        
+
+    bboxes = []
+    labels = []
+    for ann in annotates:       # outer loop is loop over images
+        img_bboxes = []
+        img_labels = []
+        for detection in ann:   # inner loop is loop over detections in an image
+            bbox = detection["bbox"]
+            cat_id = detection["category_id"]
+
+            bbox[2] = max(bbox[2], 1)   # clip width and height to 1
+            bbox[3] = max(bbox[3], 1)
+        
+            img_bboxes.append(bbox)
+            img_labels.append(id_to_label[cat_id])
+
+        bboxes.append(img_bboxes)
+        labels.append(img_labels)
+
+    detection = {
+        "img_ids": img_ids,
+        "img_names": img_names,
+        "bboxes": bboxes,
+        "labels": labels
+    }
+    with open(detection_file, "wb") as f:
+        pickle.dump(detection, f)          # save to disk
+
+    del coco
 
 class COCODataset(Dataset):
-    def __init__(
-        self, 
-        data_dir: str, 
-        data_name: str, 
-        transforms=None, 
-        img_width: int=512, 
-        img_height: int=512,
-        eval: bool=False
-        ):
+    def __init__(self, data_dir: str, coco_version: str, transforms: A.Compose = None):
         super(COCODataset, self).__init__()
-
-        detection_file = os.path.join(data_dir, "annotations", data_name, "detections.pkl")
-        label_map_file = os.path.join(data_dir, "annotations", data_name, "label_map.pkl")
-        id_map_file = os.path.join(data_dir, "annotations", data_name, "id_map.pkl")
+        detection_file = os.path.join(data_dir, "annotations", coco_version, "detections.pkl")
+        
         with open(detection_file, "rb") as f:
             detection = pickle.load(f)
 
-        with open(label_map_file, "rb") as f:
-            label_to_name = pickle.load(f)
+        if transforms == None:
+            warnings.warn("transforms is not specified. Default to normalize with ImageNet and resize to 512x512")
+            # use Albumentation resize to handle bbox resizing also
+            # centernet resize input to 512 and 512
+            transforms = A.Compose([
+                A.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD, max_pixel_value=255),
+                A.Resize(512, 512),
+                ToTensorV2()
+            ], bbox_params=A.BboxParams(format='coco', min_area=1024, min_visibility=0.1, label_fields=["labels"]))
 
-        with open(id_map_file, "rb") as f:
-            id_to_label = pickle.load(f)
-
-        self.eval = eval
-        if eval:
-            self.img_ids = detection["img_ids"]
-            self.original_widths = detection["img_widths"]
-            self.original_heights = detection["img_heights"]
-        
+        self.img_dir = os.path.join(data_dir, coco_version)
         self.img_names = detection["img_names"]
         self.bboxes = detection["bboxes"]
         self.labels = detection["labels"]
-        self.label_to_name = label_to_name
-        self.id_to_label = id_to_label
-        self.num_classes = len(label_to_name)
-        self.img_dir = os.path.join(data_dir, data_name)
-
-        # default transforms is convert to tensor
-        if transforms == None:
-            # use Albumentation resize to handle bbox resizing also
-            # centernet resize input to 512 and 512
-            # yolo bbox format is cxcywh
-            transforms = A.Compose([
-                A.Resize(img_height, img_width),
-                ToTensorV2()
-            ], bbox_params=A.BboxParams(format='coco', min_area=1024, min_visibility=0.1, label_fields=["labels"]))
-            
         self.transforms = transforms
-        self.img_width = img_width
-        self.img_height = img_height
-
 
     def __getitem__(self, index: int):
-        # coco data format https://cocodataset.org/#format-data
         img_path = os.path.join(self.img_dir, self.img_names[index])
-
-        # read image with cv2. convert to rgb color
         img = cv2.imread(img_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -183,61 +119,104 @@ class COCODataset(Dataset):
         # self.transforms is an Albumentations Transform instance
         # Albumentations will handle transforming the bounding boxes also
         augmented = self.transforms(image=img, bboxes=bboxes, labels=labels)
-        img = augmented["image"] / 255.     # don't know why Albumentations doesn't divide by 255 by default
+        img = augmented["image"]
         bboxes = augmented["bboxes"]
         labels = augmented["labels"]
 
-        for i in range(len(bboxes)):
-            x,y,w,h = bboxes[i]
-            # clip boxes
-            # x = max(x,0)
-            # y = max(y,0)
-            if x < 0:
-                w = w + x
-                x = 0
-            if y < 0:
-                h = h + y
-                y = 0
-            w = min(w, self.img_width-x)
-            h = min(h, self.img_height-y)
+        for i, box in enumerate(bboxes):
+            x, y, w, h = box
+            cx = x + w / 2
+            cy = y + h / 2
+            bboxes[i] = [cx, cy, w, h]
 
-            # convert COCO xywh to cxcywh and convert to relative scale
-            center_x = (x + w/2) / self.img_width
-            center_y = (y + h/2) / self.img_height
-            w = w / self.img_width
-            h = h / self.img_height
-
-            bboxes[i] = [center_x, center_y, w, h]
-
-        data = {
+        item = {
             "image": img,
             "bboxes": bboxes,
             "labels": labels
         }
-        if self.eval:
-            data["img_ids"] = self.img_ids[index]
-            data["original_widths"] = self.original_widths[index]
-            data["original_heights"] = self.original_heights[index]
-        
-        return data
+        return item
 
     def __len__(self):
         return len(self.img_names)
 
-def collate_detections_with_padding(batch, pad_value=0):
+class COCODataModule(pl.LightningDataModule):
+    """Lightning Data Module, used for training
+    """
+    def __init__(self, train: Dict, validation: Dict = None, test: Dict = None):
+        super().__init__()
+        self.train_cfg = train
+        self.val_cfg = validation
+        self.test_cfg = test
+
+        self.train_transforms = self._parse_transforms(train["transforms"])
+        self.val_transforms = self._parse_transforms(validation["transforms"]) if validation else None
+        self.test_transforms = self._parse_transforms(test["transforms"]) if test else None
+
+    def _parse_transforms(self, transforms_cfg):
+        transforms = []
+        for x in transforms_cfg:
+            transf = A.__dict__[x["name"]](**x["params"])
+            transforms.append(transf)
+
+        transforms.append(A.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD, max_pixel_value=255))
+        transforms.append(ToTensorV2())
+        transforms = A.Compose(
+            transforms,
+            bbox_params=A.BboxParams(format='coco', min_area=1024, min_visibility=0.1, label_fields=["labels"])
+        )
+        return transforms
+
+    def prepare_data(self):
+        prepare_coco_detection(self.train_cfg["data_dir"], self.train_cfg["coco_version"])
+        prepare_coco_detection(self.val_cfg["data_dir"], self.val_cfg["coco_version"]) if self.val_cfg else None
+
+    def setup(self, stage: str):
+        if stage in (None, "fit"):
+            self.coco_train = COCODataset(
+                self.train_cfg["data_dir"],
+                self.train_cfg["coco_version"],
+                transforms=self.train_transforms
+            )
+            self.coco_val = COCODataset(
+                self.val_cfg["data_dir"],
+                self.val_cfg["coco_version"],
+                transforms=self.val_transforms
+            )
+
+    def train_dataloader(self):
+        train_dataloader = DataLoader(
+            self.coco_train,
+            batch_size=self.train_cfg["batch_size"],
+            shuffle=True,
+            num_workers=4,
+            collate_fn=collate_detections_with_padding,
+            pin_memory=True
+        )
+        return train_dataloader
+
+    def val_dataloader(self):
+        val_dataloader = DataLoader(
+            self.coco_val,
+            batch_size=self.val_cfg["batch_size"],
+            shuffle=False,
+            num_workers=4,
+            collate_fn=collate_detections_with_padding,
+            pin_memory=True
+        )
+        return val_dataloader
+
+def collate_detections_with_padding(batch: Iterable[Dict[str, np.ndarray]], pad_value: int = 0):
     output = {key: [] for key in batch[0]}
     output["mask"] = []
     max_size = 0
 
+    # collate items to a list and find max number of detection in this batch
     for item in batch:
-        # output["image"].append(item["image"])
-        # output["bboxes"].append(item["bboxes"])
-        # output["labels"].append(item["labels"])
-        for key in item:
-            output[key].append(item[key])
-
+        for key, value in item.items():
+            output[key].append(value)
         max_size = max(max_size, len(item["labels"]))
     
+    # pad labels and masks to max length
     for i in range(len(batch)):
         item_size = len(output["labels"][i])
         output["mask"].append([1]*item_size)
@@ -247,12 +226,49 @@ def collate_detections_with_padding(batch, pad_value=0):
             output["labels"][i].append(pad_value)
             output["mask"][i].append(0)    
     
-    output["image"] = torch.stack(output["image"], dim=0)
-    # output["bboxes"] = torch.tensor(output["bboxes"])
-    # output["labels"] = torch.tensor(output["labels"])
-    # output["mask"] = torch.tensor(output["mask"])
+    # image is a list of tensor -> use torch.stack
+    # the rest are nested lists -> use torch.tensor
     for key, value in output.items():
         if key != "image":
             output[key] = torch.tensor(value)
-    
+        else:
+            output[key] = torch.stack(value, dim=0)
+
     return output
+
+class InferenceDataset(Dataset):
+    """Dataset used for inference. Each item is a dict with keys `image`, `original_height`, and `original_width`.
+    """
+    def __init__(self, data_dir: str, resize_height: int = 512, resize_width: int = 512):
+        assert os.path.exists(data_dir), f"{data_dir} does not exist"
+
+        supported_formats = ["bmp", "jpeg", "jpg", "png"]   # cv2 is used to read the image. add image extension here if you need to read other formats
+        files = [x for x in os.listdir(data_dir) if x.split(".")[-1].lower() in supported_formats]
+
+        transforms = A.Compose([
+            A.Resize(height=resize_height, width=resize_width),
+            A.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD, max_pixel_value=255),
+            ToTensorV2()
+        ])
+
+        self.data_dir = data_dir
+        self.img_names = files
+        self.transforms = transforms
+
+    def __getitem__(self, index: int):
+        img = os.path.join(self.data_dir, self.img_names[index])
+        img = cv2.imread(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        height, width, _ = img.shape
+        img = self.transforms(image=img)["image"]
+
+        item = {
+            "image": img,
+            "original_height": height,
+            "original_width": width
+        }
+        return item
+    
+    def __len__(self):
+        return len(self.img_names)
--- datasets_test.py
+++ datasets_test.py
@@ -1,32 +1,32 @@
-import os, random
+import os
+import random
 import torch
 from torch.utils.data import DataLoader
 from datasets import COCODataset, prepare_coco_detection, collate_detections_with_padding
-from train import get_train_augmentations
 
-
-DATASET_ROOT = os.path.join("datasets")
-COCO_DIR = os.path.join(DATASET_ROOT, "COCO")
+COCO_DIR = os.path.join("datasets", "COCO")
 
 class TestDatasets:
-    DATA_KEYS = ["image", "bboxes", "labels"]
-
     def test_prepare_coco_detection(self):
         prepare_coco_detection(COCO_DIR, "val2017")
 
-    def test_coco(self):
+        export_dir = os.path.join(COCO_DIR, "annotations", "val2017")
+        assert os.path.exists(os.path.join(export_dir, "detections.pkl"))
+        assert os.path.exists(os.path.join(export_dir, "label_to_name.json"))
+        assert os.path.exists(os.path.join(export_dir, "label_to_id.json"))
+
+    def test_coco_dataset(self):
         ds = COCODataset(COCO_DIR, "val2017")
 
         rand_idx = random.randint(0,len(ds)-1)
         sample = ds[rand_idx]
-        for x in self.DATA_KEYS:
+        for x in ["image", "bboxes", "labels"]:
             assert x in sample
 
         img = sample["image"]
         assert type(img) == torch.Tensor    # torch tensor
-        assert len(img.shape) == 3          # rank 3 tensor
         assert img.shape == (3,512,512)     # check for correct default size
-        assert torch.max(img) <= 1          # normalize to [0,1]
+        assert torch.max(torch.abs(img)) <= 3
 
         bboxes = sample["bboxes"]
         for box in bboxes:
@@ -34,23 +34,16 @@ class TestDatasets:
         
         labels = sample["labels"]
         assert len(bboxes) == len(labels)
-
-    def test_augmentation(self):
-        train_augment = get_train_augmentations()
-        ds = COCODataset(COCO_DIR, "val2017", transforms=train_augment)
-
-        rand_idx = random.randint(0,len(ds)-1)
-        sample1 = ds[rand_idx]["image"]
-        sample2 = ds[rand_idx]["image"]
-        diff = sample1 - sample2
-        assert (diff*diff).sum() > 1e-10        # due to augmentations, they should be different
-        assert sample1.shape == (3,512,512)     # size is still the same
+        for label in labels:
+            assert label < 81
 
     def test_collate_fn(self):
         ds = COCODataset(COCO_DIR, "val2017")
-        coco_dataloader = DataLoader(ds, batch_size=4, num_workers=2, collate_fn=collate_detections_with_padding)
+        coco_dataloader = DataLoader(ds, batch_size=4, collate_fn=collate_detections_with_padding)
         batch = next(iter(coco_dataloader))
 
-        for x in self.DATA_KEYS + ["mask"]:
+        for x in ["image", "bboxes", "labels", "mask"]:
             assert x in batch
             assert type(batch[x]) == torch.Tensor
+
+        assert batch["image"].shape == (4,3,512,512)
