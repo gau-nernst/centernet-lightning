e5c1ae0d1788fc3664d21960088c273cb833f61b
Thien Tran <gau.nernst@yahoo.com.sg>
2021-06-22 14:50:52 +0800

    remove reduction option

--- src/losses/focal_loss.py
+++ src/losses/focal_loss.py
@@ -8,13 +8,10 @@ class FocalLossWithLogits(nn.Module):
     # reference implementations
     # https://github.com/xingyizhou/CenterTrack/blob/master/src/lib/model/losses.py#L72
     # default alpha and beta values taken from CenterTrack
-    def __init__(self, alpha: float=2., beta: float=4., reduction="mean"):
+    def __init__(self, alpha: float=2., beta: float=4.):
         super(FocalLossWithLogits, self).__init__()
-        assert reduction in ("mean", "sum", "none")
         self.alpha = alpha
         self.beta = beta
-        self.reduction = reduction
-        
     
     def forward(self, inputs: torch.Tensor, targets: torch.Tensor):
         # NOTE: targets is a 2D Gaussian
@@ -27,14 +24,8 @@ class FocalLossWithLogits(nn.Module):
         pos_loss = -F.logsigmoid(inputs) * (1-probs)**self.alpha * pos_mask                         # loss at Gaussian peak
         neg_loss = -F.logsigmoid(-inputs) * probs**self.alpha * (1-targets)**self.beta * neg_mask   # loss at everywhere else
 
-        if self.reduction == "none":
-            return pos_loss + neg_loss
-
         pos_loss = pos_loss.sum()
         neg_loss = neg_loss.sum()
-
-        if self.reduction == "sum":
-            return pos_loss + neg_loss
 
         N = pos_mask.sum()  # number of peaks = number of ground-truth detections
         # use N + eps instead of 2 cases?
