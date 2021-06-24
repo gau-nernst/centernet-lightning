4a367559add651a54fae35a36e3ee9c240cea65c
Thien Tran <gau.nernst@yahoo.com.sg>
2021-06-24 09:55:29 +0800

    include collate function in datasets init

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,11 +1,11 @@
-from .utils import IMAGENET_MEAN, IMAGENET_STD, render_target_heatmap_cornernet, render_target_heatmap_ttfnet
+from .utils import IMAGENET_MEAN, IMAGENET_STD, render_target_heatmap_cornernet, render_target_heatmap_ttfnet, CollateDetectionsCenterNet
 from .coco import COCODataset
 from .voc import VOCDataset
 from .inference import InferenceDataset
 from .builder import build_dataset, build_dataloader
 
 __all__ = [
-    "IMAGENET_MEAN", "IMAGENET_STD", "render_target_heatmap_cornernet", "render_target_heatmap_ttfnet",
+    "IMAGENET_MEAN", "IMAGENET_STD", "render_target_heatmap_cornernet", "render_target_heatmap_ttfnet", "CollateDetectionsCenterNet"
     "COCODataset", "VOCDataset", "InferenceDataset",
     "build_dataset", "build_dataloader"
 ]
