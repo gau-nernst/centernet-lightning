b194d3435d2a90eda5ff296feb01f11c1d3a6181
Thien Tran <gau.nernst@yahoo.com.sg>
2021-06-24 16:27:48 +0800

    fix typo

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -25,16 +25,16 @@ class TestLosses:
         assert heatmap[labels[0], bboxes[0][1], bboxes[0][0]] == 1    # peak is 1
         assert np.sum(heatmap == 1) == len(labels)
 
-    def test_focal_loss(self):
-        # sample_output = torch.randn((4,3,128,128)) * 10 - 5
-        # sample_logits = -torch.log((1-sample_output) / (sample_output+1e-8))
+    # def test_focal_loss(self):
+    #     sample_output = torch.randn((4,3,128,128)) * 10 - 5
+    #     sample_logits = -torch.log((1-sample_output) / (sample_output+1e-8))
 
-        # sample_target = torch.rand((4,3,128,128))
-        # sample_target[0,0,64,64] = 1
-        # sample_target[0,1,80,72] = 1
+    #     sample_target = torch.rand((4,3,128,128))
+    #     sample_target[0,0,64,64] = 1
+    #     sample_target[0,1,80,72] = 1
 
-        # focal_loss = FocalLossWithLogits(alpha=2, beta=4)
+    #     focal_loss = FocalLossWithLogits(alpha=2, beta=4)
 
-        # loss1 = focal_loss(sample_logits, sample_target)
-        # loss2 = reference_focal_loss(sample_output, sample_target)
-        # assert torch.abs(loss1 - loss2) < 1e-3
+    #     loss1 = focal_loss(sample_logits, sample_target)
+    #     loss2 = reference_focal_loss(sample_output, sample_target)
+    #     assert torch.abs(loss1 - loss2) < 1e-3
