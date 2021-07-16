b9010977289b67040b63dbd2b9105b80b95bdd5b
Thien Tran <gau.nernst@yahoo.com.sg>
2021-07-16 16:58:07 +0800

    fix issue with box out of frame for crowdhuman
    - use PIL Image to read image dimensions
    - this does not load image to memory

--- src/datasets/crowdhuman.py
+++ src/datasets/crowdhuman.py
@@ -3,6 +3,7 @@ import json
 
 from torch.utils.data import Dataset
 import cv2
+from PIL import Image
 
 class CrowdHumanDataset(Dataset):
     def __init__(self, data_dir, split, transforms=None, name_to_label={"person": 0, "mask": 1}, img_ext=".jpg"):
@@ -22,21 +23,35 @@ class CrowdHumanDataset(Dataset):
         self.bboxes = []
         for line in annotations:
             img_name = f"{line['ID']}{img_ext}"
+            
+            # annotations do not provide image dimensions
+            # seems like PIL can read image dimensions without loading the image to memory
+            img_path = os.path.join(self.img_dir, img_name)
+            img = Image.open(img_path)
+            img_width = img.width
+            img_height = img.height
+            
             img_labels = []
             img_bboxes = []
             for detection in line["gtboxes"]:
                 label = name_to_label[detection["tag"]]
-                
                 x, y, w, h = detection["fbox"]
-                if w < 1 or h < 1:
-                    continue
-                # since image width and height are not present in annotations
-                # we can only clip x2 y2 in __getitem__()
+
                 x2 = x + w
                 y2 = y + h
                 x1 = max(0, x)
                 y1 = max(0, y)
-                box = [x1, y1, x2, y2]
+                x2 = min(img_width-1, x2)
+                y2 = min(img_height-1, y2)
+
+                if x2 - x1 < 1 or y2 - y1 < 1:
+                    continue
+
+                cx = (x1 + x2) / 2 / img_width
+                cy = (y1 + y2) / 2 / img_height
+                w = (x2 - x1) / img_width
+                h = (y2 - y1) / img_height
+                box = [cx,cy,w,h]
 
                 img_labels.append(label)
                 img_bboxes.append(box)
@@ -49,19 +64,9 @@ class CrowdHumanDataset(Dataset):
         img = os.path.join(self.img_dir, self.img_names[index])
         img = cv2.imread(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img_height, img_width, _ = img.shape
 
         labels = self.labels[index]
         bboxes = self.bboxes[index]
-
-        for box in bboxes:
-            box[2] = min(img_width-1, box[2])
-            box[3] = min(img_height-1, box[3])
-
-            box[2] = (box[2] - box[0]) / img_width      # w = x2 - x1
-            box[3] = (box[3] - box[1]) / img_height     # h = y2 - y1
-            box[0] = box[0] / img_width + box[2] / 2    # cx = x1 + w/2
-            box[1] = box[1] / img_height + box[3] / 2   # cy = y1 + h/2
 
         if self.transforms is not None:
             augmented = self.transforms(image=img, bboxes=bboxes, labels=labels)
