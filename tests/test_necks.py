f103e784b283a3d2ca04087928d322bf96c2149e
Thien Tran <gau.nernst@yahoo.com.sg>
2021-08-16 11:52:30 +0800

    update tests to use fixtures

--- tests/test_necks.py
+++ tests/test_necks.py
@@ -1,57 +1,62 @@
+import pytest
 import torch
 
 from centernet_lightning.models.necks import SimpleNeck, FPNNeck, IDANeck, BiFPNNeck, build_neck
 
-test_config = "configs/test_config.yaml"
+@pytest.fixture
+def backbone_channels():
+    return [64, 64, 128, 256, 512]
+
+@pytest.fixture
+def upsample_channels():
+    return [256,128,64]
+
+@pytest.fixture
+def sample_input_single():
+    return torch.rand((4,512,16,16))
+
+@pytest.fixture
+def sample_input_multiple(backbone_channels):
+    dims = [512 // 2**(i+1) for i in range(len(backbone_channels))]
+    return [torch.rand((4,c,d,d)) for c,d in zip(backbone_channels, dims)]
 
 class TestSimpleNeck:
-    def test_construction(self):
-        neck = SimpleNeck([1024], upsample_channels=[256,128,64])
-        assert neck.out_channels == 64
-        assert neck.upsample_stride == 8
+    def test_attributes(self, backbone_channels, upsample_channels):
+        neck = SimpleNeck(backbone_channels, upsample_channels)
+       
+        assert neck.out_channels == upsample_channels[-1]
+        assert neck.upsample_stride == 2**len(upsample_channels)
 
-    def test_forward(self):
-        neck = SimpleNeck([1024], upsample_channels=[256,128,64])
-        outputs = neck(torch.rand((4,1024,16,16)))
-        assert isinstance(outputs, torch.Tensor)
-        assert outputs.shape == (4,64,128,128)
+    def test_forward(self, backbone_channels, upsample_channels, sample_input_single):
+        neck = SimpleNeck(backbone_channels, upsample_channels)
+        output = neck(sample_input_single)
+
+        assert isinstance(output, torch.Tensor)
+        assert output.shape[0] == sample_input_single.shape[0]
+        assert output.shape[1] == neck.out_channels
+        assert output.shape[2] == sample_input_single.shape[2] * neck.upsample_stride
+        assert output.shape[3] == sample_input_single.shape[3] * neck.upsample_stride
 
 class TestFPNNeck:
-    def test_construction(self):
-        neck = FPNNeck([64,128,256,512,1024], upsample_channels=[256,128,64])
-        assert neck.out_channels == 64
-        assert neck.upsample_stride == 8
+    def test_attributes(self, backbone_channels, upsample_channels):
+        neck = FPNNeck(backbone_channels, upsample_channels)
 
-    def test_forward(self):
-        neck = FPNNeck([64,128,256,512,1024], upsample_channels=[256,128,64])
-        inputs = [
-            torch.rand((4,128,128,128)),
-            torch.rand((4,256,64,64)),
-            torch.rand((4,512,32,32)),
-            torch.rand((4,1024,16,16))
-        ]
-        outputs = neck(inputs)
-        assert isinstance(outputs, torch.Tensor)
-        assert outputs.shape == (4,64,128,128)
+        assert neck.out_channels == upsample_channels[-1]
+        assert neck.upsample_stride == 2**len(upsample_channels)
 
-class TestBuilder:
-    def test_build_from_file(self):
-        neck = build_neck(test_config, backbone_channels=[64,128,256,512,1024])
-        assert isinstance(neck, SimpleNeck)
+    def test_forward(self, backbone_channels, upsample_channels, sample_input_multiple):
+        last_input = sample_input_multiple[-1]
+        neck = FPNNeck(backbone_channels, upsample_channels)
+        output = neck(sample_input_multiple)
+        
+        assert isinstance(output, torch.Tensor)
+        assert output.shape[0] == last_input.shape[0]
+        assert output.shape[1] == neck.out_channels
+        assert output.shape[2] == last_input.shape[2] * neck.upsample_stride
+        assert output.shape[3] == last_input.shape[3] * neck.upsample_stride
 
-    def test_build_from_dict(self):
-        config = {"name": "simple"}
-        neck = build_neck(config, backbone_channels=[1024])
-        assert isinstance(neck, SimpleNeck)
+class TestBiFPNNeck:
+    pass
 
-        config = {"name": "fpn"}
-        neck = build_neck(config, backbone_channels=[64,128,256,512,1024])
-        assert isinstance(neck, FPNNeck)
-
-        config = {"name": "ida"}
-        neck = build_neck(config, backbone_channels=[64,128,256,512,1024])
-        assert isinstance(neck, IDANeck)
-
-        config = {"name": "bifpn"}
-        neck = build_neck(config, backbone_channels=[64,128,256,512,1024])
-        assert isinstance(neck, BiFPNNeck)
+class TestIDANeck:
+    pass
