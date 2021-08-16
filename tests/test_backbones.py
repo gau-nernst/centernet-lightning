de5c9ecad0fa366ccbb79e41d07ecd539fc24af8
Thien Tran <gau.nernst@yahoo.com.sg>
2021-08-16 10:33:03 +0800

    update backbone tests to use test generation

--- tests/test_backbones.py
+++ tests/test_backbones.py
@@ -1,66 +1,86 @@
+import pytest
 import torch
-from centernet_lightning.models.backbones import ResNetBackbone, MobileNetBackbone, build_backbone
+import yaml
 
-_backbones = [
-    {
-        "constructor": ResNetBackbone,
-        "names": ["resnet18", "resnet34", "resnet50", "resnet101"],
-        "output_stride": 32
-    },
-    {
-        "constructor": MobileNetBackbone,
-        "names": ["mobilenet_v2", "mobilenet_v3_large", "mobilenet_v3_small"],
-        "output_stride": 32
-    }
-]
+from centernet_lightning.models.backbones import ResNetBackbone, MobileNetBackbone, TimmBackbone, build_backbone
 
-test_config = "configs/test_config.yaml"
+@pytest.fixture
+def sample_input():
+    return torch.rand((4,3,512,512))
 
-class TestConvUpBlock:
-    pass
+def pytest_generate_tests(metafunc):
+    ids = []
+    arg_names = list(metafunc.cls.backbones[0][1].keys())
+    arg_values = []
 
-class TestBackbones:
-    def test_construction(self):
-        for backbone_type in _backbones:
-            constructor = backbone_type["constructor"]
-            names = backbone_type["names"]
+    for backbone in metafunc.cls.backbones:
+        backbone_id, args = backbone
+        ids.append(backbone_id)
+        arg_values.append(args.values())
+    
+    metafunc.parametrize(arg_names, arg_values, ids=ids, scope="class")
 
-            for name in names:
-                model = constructor(name)
-                assert model.output_stride == backbone_type["output_stride"]
-                assert isinstance(model.out_channels, list)
+def generate_backbones():
+    resnet_backbones = [(f"resnet{x}", {
+        "constructor": ResNetBackbone, 
+        "name": f"resnet{x}",
+    }) for x in (18,34,50,101)]
 
-    def test_forward_single_output(self):
-        for backbone_type in _backbones:
-            constructor = backbone_type["constructor"]
-            names = backbone_type["names"]
+    mobilenet_backbones = [(f"mobilenet_{x}", {
+        "constructor": MobileNetBackbone, 
+        "name": f"mobilenet_{x}",
+    }) for x in ("v2", "v3_small", "v3_large")]
 
-            model = constructor(names[0])
-            outputs = model(torch.rand((4,3,224,224)))
-            assert isinstance(outputs, torch.Tensor)
+    timm_backbones = [(f"timm_{name}", {
+        "constructor": TimmBackbone, 
+        "name": name,
+    }) for name in ["resnet18", "efficientnet_b0"]]
 
-    def test_forward_multiple_outputs(self):
-        for backbone_type in _backbones:
-            constructor = backbone_type["constructor"]
-            names = backbone_type["names"]
+    return resnet_backbones + mobilenet_backbones + timm_backbones
 
-            model = constructor(names[0], return_features=True)
-            outputs = model(torch.rand((4,3,224,224)))
-            assert isinstance(outputs, list)
-            for out in outputs:
-                assert isinstance(out, torch.Tensor)
+class TestBackbone:
+    backbones = generate_backbones()
 
-class TestBuilder:
-    def test_build_from_file(self):
-        backbone = build_backbone(test_config)
-        assert isinstance(backbone, ResNetBackbone)
+    def test_attributes(self, constructor, name):
+        model = constructor(name)
+        
+        assert hasattr(model, "output_stride")
+        assert hasattr(model, "out_channels")
 
-    def test_build_from_dict(self):
-        for backbone_type in _backbones:
-            constructor = backbone_type["constructor"]
-            names = backbone_type["names"]
-            config = {
-                "name": names[0]
-            }
-            model = build_backbone(config)
-            assert isinstance(model, constructor)
+    def test_forward_single(self, constructor, name, sample_input):
+        model = constructor(name, return_features=False)
+        output = model(sample_input)
+
+        assert isinstance(output, torch.Tensor)
+        assert output.shape[0] == sample_input.shape[0]
+        assert output.shape[1] == model.out_channels[-1]
+        assert output.shape[2] == sample_input.shape[2] // model.output_stride
+        assert output.shape[3] == sample_input.shape[3] // model.output_stride
+
+    def test_forward_multiple(self, constructor, name, sample_input):
+        model = constructor(name, return_features=True)
+        output = model(sample_input)
+        
+        assert isinstance(output, list)
+        for i, (features, n_channels) in enumerate(zip(output, model.out_channels)):
+            assert isinstance(features, torch.Tensor)
+            assert features.shape[0] == sample_input.shape[0]
+            assert features.shape[1] == n_channels
+            assert features.shape[2] == sample_input.shape[2] // 2**(i+1)
+            assert features.shape[3] == sample_input.shape[3] // 2**(i+1)
+
+    def test_builder(self, constructor, name, tmp_path):
+        if constructor == TimmBackbone:
+            name = f"timm_{name}"
+
+        backbone_config = {"name": name, "pretrained": True}
+        backbone = build_backbone(backbone_config)
+        assert isinstance(backbone, constructor)
+
+        backbone_yaml = {"model": {"backbone": backbone_config}}
+        config_path = tmp_path / "config.yaml"
+        with config_path.open("w") as f:
+            yaml.dump(backbone_yaml, f)
+        backbone = build_backbone(str(config_path))
+        assert isinstance(backbone, constructor)
+    
