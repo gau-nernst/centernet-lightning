18d0f6d5a9e0a2b8b74f11663292ead25527cbb7
Thien Tran <gau.nernst@yahoo.com.sg>
2022-01-30 22:25:39 +0800

    add placeholder for trivial augment

--- centernet_lightning/datasets/transforms.py
+++ centernet_lightning/datasets/transforms.py
@@ -1,11 +1,19 @@
 from typing import Tuple, Dict, Union
 
 import torch
+import albumentations as A
+
+class TrivialAugmentWide(A.Sequential):
+    def __init__(self):
+        transforms = [
+
+        ]
+        super().__init__(transforms)
 
 
 class Mosaic:
     def __init__(self):
-        super().__init__()
+        pass
 
     def __call__(self, images: torch.Tensor, targets: Tuple[Dict[str, Union[Tuple, int]]]):
         pass
