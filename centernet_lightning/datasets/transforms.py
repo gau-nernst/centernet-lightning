9ce935d958ed033c14fda9d1a5c54c1b8a2c04a0
Thien Tran <gau.nernst@yahoo.com.sg>
2022-02-12 22:39:26 +0800

    add trivial augment with Albumentations

--- centernet_lightning/datasets/transforms.py
+++ centernet_lightning/datasets/transforms.py
@@ -3,12 +3,27 @@ from typing import Tuple, Dict, Union
 import torch
 import albumentations as A
 
-class TrivialAugmentWide(A.Sequential):
+
+# https://github.com/pytorch/vision/blob/main/torchvision/transforms/autoaugment.py
+class TrivialAugmentWide(A.OneOf):
     def __init__(self):
         transforms = [
-
+            A.Affine(shear={'x': 45, 'y': 0}),
+            A.Affine(shear={'x': 0, 'y': 45}),
+            A.Affine(translate_px={'x': 32, 'y': 0}),
+            A.Affine(translate_px={'x': 0, 'y': 32}),
+            A.Affine(rotate=135),
+            A.ColorJitter(brightness=0.99, contrast=0, saturation=0, hue=0),
+            A.ColorJitter(brightness=0, contrast=0.99, saturation=0, hue=0),
+            A.ColorJitter(brightness=0, contrast=0, saturation=0.99, hue=0),
+            A.Sharpen(alpha=(0, 0.99), lightness=(1,1)),
+            A.Posterize(num_bits=(2, 8)),
+            A.Solarize(threshold=(0, 255)),
+            # no autoconstrast
+            A.Equalize()
         ]
-        super().__init__(transforms)
+        num_t = len(transforms)
+        super().__init__(transforms, p=num_t/(num_t+1))
 
 
 class Mosaic:
